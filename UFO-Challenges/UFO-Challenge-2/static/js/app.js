44a33ee3b9596e2e7514c33db199560815e92b16
georgealym <georgealym@gmail.com>
2020-09-13 13:20:24 -0400

    commenting UFO challenge two

---
+++ UFO-Challenges/UFO-Challenge-2/static/js/app.js
@@ -0,0 +1,54 @@
+// from data.js
+var tableData = data;
+//appends data from table using keys at column names and adding values to table itself with all of the different
+//titles of columns
+var table = d3.select("tbody");
+function buildtable(data) {
+    table.html("");
+    data.forEach((filter) => {
+        var rowinfo = table.append("tr");
+        Object.entries(filter).forEach(([key, value]) => {
+          var cell = rowinfo.append("td");
+          cell.text(value);
+        });
+});
+}
+buildtable(tableData) 
+
+//create a function for multple filters to take the input and find the data associated with it
+function multiplefilter() {
+  //select the use input
+  var element = d3.select(this).select("input");
+  //find the value associated with the input 
+  var item = element.property("value");
+  var id = element.attr("id");
+  //if the value matches the filter id in the table, show only that information and do not show the other information matching with it 
+  if (item) {
+    filters[id] = item;
+  }
+  else {
+    delete filters[id];
+  }
+  //when you click the button
+  buttonclick();
+}
+
+
+//add a function for when you click the button 
+function buttonclick(){
+    d3.event.preventDefault();
+    //create variables using the user input from the filtered data
+    var filtered = tableData;
+    //uses the entries from the different filters to determine what to search for using the keys and values 
+    Object.entries(filters).forEach(([key, value]) => {
+      filtered = filtered.filter(row => row[key] === value)
+    });
+      buildtable(filtered);
+  };
+  //uses d3 on the class of the button in the HTML to run this function when the button is clicked on 
+  //the webpage
+  d3.selectAll("#filter-btn").on("click", buttonclick);
+  //uses d3 to change the filter 
+  d3.selectAll(".filter").on("change", multiplefilter);
+
+
